54f5763d53a65fb47ccf3e3632acf6552844ff57
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2023-06-18 12:41:48 +0530

    nodemon install

--- routes/BlogRoutes.js
+++ routes/BlogRoutes.js
@@ -8,6 +8,4 @@ router.post("/" ,blogController.blog_create_post);
 router.get('/:id', blogController.blog_details);
 router.delete('/:id' , blogController.blog_delete);
 
-
-
 module.exports=router;
